16424ac6424cf31b0e51e90caa13cc9da2def38a
zqg <zqg>
2020-09-06 21:02:06 +0800

    url && util module

--- node_/node_2.js
+++ node_/node_2.js
@@ -1,2 +1,17 @@
 const url_ = require('url')
-console.log(url_);
+// console.log(url_);
+console.log(url_.parse("http://localhost:8888/start?foo=bar&hello=world").query);
+
+
+const util = require('util');
+const { url } = require('inspector');
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+    this.showName = function(){
+        console.log(this.name);
+    }
+}
+let p = new Person('zs',20)
+console.log(p);
+console.log(util.inspect(p));
