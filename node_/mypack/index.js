83cf60c1a03ed09c57c4fb005303c43c04d97bfd
zqg <zqg>
2020-09-07 11:27:17 +0800

    add Object querystring.parse(String params) method using instance

--- node_/mypack/index.js
+++ node_/mypack/index.js
@@ -1,3 +1,23 @@
+const querystring =  require('querystring')
+const http =  require('http')
 const ejs = require('ejs')
 
-console.log(ejs);
+
+let queryParams = 'foo=bar&abc=xyz&abc=123';
+console.log(querystring.parse(queryParams)); //=> { foo: 'bar', abc: [ 'xyz', '123' ] }
+
+
+let server = http.createServer((request, response) => {
+    console.log(request.url);
+    console.log("---request start-----");
+    response.end('200 ok');
+    console.log("---response end------");
+})
+
+
+
+
+server.listen(8888, () => {
+    // console.log(ejs);
+    console.log('-----server start----');
+})
