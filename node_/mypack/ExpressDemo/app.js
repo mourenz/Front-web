6e975421dee2803097b43f78c30b60a889ddfc67
zqg <zqg>
2020-09-07 17:14:31 +0800

    add module Router

--- node_/mypack/ExpressDemo/app.js
+++ node_/mypack/ExpressDemo/app.js
@@ -1,14 +1,32 @@
 const express = require('express')
+// 模块导入
+const departmentRouter = require('./route/department')
+const userRouter = require('./route/user')
+
 const app = express();
 
-app.listen(8888,()=>{
+app.listen(8888, () => {
     console.log("启动成功....")
 })
 
-app.get('/',(req,resp)=>{
+// 原生
+app.get('/', (req, resp) => {
     resp.send('Hello express')
 })
 
-app.post('/post',(req,resp)=>{
+app.post('/post', (req, resp) => {
     resp.send('Hello express POST')
 })
+
+app.route("/route")
+    .get((req, resp) => {
+        resp.send("get method.....");
+    })
+    .post((req, resp) => {
+    resp.send("post method.......");
+})
+
+
+// 应用模块方法  模块的方法需要 module.exports = xxx
+app.use(departmentRouter);
+app.use(userRouter);
