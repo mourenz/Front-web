0d805ec9c9213972bd07e47f2f7bedc011eaf56a
zqg <zqg>
2020-09-07 11:14:57 +0800

    add

--- node_/node_server.js
+++ node_/node_server.js
@@ -1,11 +1,14 @@
 const http = require('http')
-let server = http.createServer((request,response)=>{
+
+let server = http.createServer((request, response) => {
+    console.log(request.url);
+    // console.log("url->"+request.url);
     console.log("---request start-----");
     response.end('200 ok');
     console.log("---response end------");
 })
 
-server.listen(8888,()=>{
+server.listen(8888, () => {
     console.log('-----server start----');
 })
 
