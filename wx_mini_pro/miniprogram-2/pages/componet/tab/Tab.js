438b42be9b81e67825656171532559215364a071
zqg <zqg.qq.com>
2020-11-30 23:54:27 +0800

    add

--- wx_mini_pro/miniprogram-2/pages/componet/tab/Tab.js
+++ wx_mini_pro/miniprogram-2/pages/componet/tab/Tab.js
@@ -11,6 +11,20 @@ Component({
    * 组件的初始数据
    */
   data: {
+    list:[
+      {
+        id:1,
+        name:"首页",
+        isActive:true
+      },
+      {
+        id:2,
+        name:"分类",
+        isActive:false
+      },
+
+
+    ]
 
   },
 
@@ -18,6 +32,24 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    handleClick:function(e){
+      // console.log(e);
+      let {index} =e.currentTarget.dataset; // 说明 dataset 是对象
+      // console.log(index);
+      let {list} = this.data;
+      console.log('index',index);
+      list.forEach((v,i)=>{
+        index === i?v.isActive=true:v.isActive=false
+        this.setData({
+          list
+        })
+      })
+
+      
+      
+
+
+    }
 
   }
 })
